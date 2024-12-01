f28d04ed5d8bf08572cc21f9b46f6b14c5333253
ap <a@p.com>
2024-12-01 09:47:15 +0100

    polishing, added multiple events

--- Prophet_bristor.py
+++ Prophet_bristor.py
@@ -5,7 +5,34 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-def fc():
+def get_past_df():
+    ### Loading
+    bristor_df = BRISTOR.load_bristor_into_df("C:\\Users\\arthu\\Downloads\\files\\BRISTOR_Zegoland.xlsx")
+
+    ### hardcoding signals
+    ds = pd.date_range(start="2020-08-01", end="2024-10-01", freq="M"),
+
+    bristor_new_patients = bristor_df[3][:44].set_index('Date')['Value'].rename('bristor_new_patients')
+    competitors_new_patients = bristor_df[3][44:87].set_index('Date')['Value'].rename('competitors_new_patients')
+    bristor_emails = bristor_df[4][:51].set_index('Date')['Value'].rename('bristor_emails')
+    bristor_call = bristor_df[4][51:].set_index('Date')['Value'].rename('bristor_call')
+    bristor_share_of_voice = bristor_df[5][:40].set_index('Date')['Value'].rename('bristor_share_of_voice')
+    competitors_share_of_voice = bristor_df[5][40:80].set_index('Date')['Value'].rename('competitors_share_of_voice')
+    bristor_factory_volumes = bristor_df[0].set_index('Date')['Value'].rename('bristor_factory_volumes')
+    bristor_demand_volumes = bristor_df[2][:46].set_index('Date')['Value'].rename('y')  # bristor_demand_volumes
+    competitors_demand_volumes = bristor_df[2][46:92].set_index('Date')['Value'].rename('competitors_demand_volumes')
+
+    df = bristor_demand_volumes.to_frame().join(competitors_new_patients, how='outer').join(bristor_emails,
+                                                                                            how='outer').join(
+        bristor_call, how='outer').join(bristor_share_of_voice, how='outer').join(competitors_share_of_voice,
+                                                                                  how='outer').join(
+        bristor_factory_volumes, how='outer').join(bristor_new_patients, how='outer').join(competitors_demand_volumes,
+                                                                                           how='outer').reset_index().rename(
+        columns={'Date': 'ds'})
+    return df
+
+
+def fc(event_start = pd.to_datetime("2025-04-01"), impact = 1.0, event_type = 'share_of_voice'):
     ### Loading
     bristor_df = BRISTOR.load_bristor_into_df("C:\\Users\\arthu\\Downloads\\files\\BRISTOR_Zegoland.xlsx")
 
@@ -63,12 +90,28 @@ def fc():
     future['bristor_new_patients'] = df['bristor_new_patients']
     future['competitors_demand_volumes'] = df['competitors_demand_volumes']
 
-    event_start = pd.to_datetime("2025-04-01")
-    impact = 0.9  # 10% reduction in demand
+    evt = 'competitors_new_patients'
+    if (event_type == 'emails'):
+        evt = 'bristor_emails'
+    elif (event_type == 'calls'):
+        evt = 'bristor_call'
+    elif (event_type == 'competitors_share_of_voice'):
+        evt = 'competitors_share_of_voice'
+    elif (event_type == 'share_of_voice'):
+        evt = 'bristor_share_of_voice'
+    elif (event_type == 'factory_volumes'):
+        evt = 'bristor_factory_volumes'
+    elif (event_type == 'new_patients'):
+        evt = 'bristor_new_patients'
+    elif (event_type == 'competitors_demand_volumes'):
+        evt = 'competitors_demand_volumes'
+
+    print(evt)
+
     # Adjust SoV for competitor impact
     for i, date in enumerate(future['ds']):  # TODO fix the share of voice
         if date >= event_start:
-            future.loc[i, 'bristor_share_of_voice'] *= impact  # Reduce SoV by 10%
+            future.loc[i, evt] *= impact  # Reduce SoV by 10%
 
     # Make predictions
     return model.predict(future)
